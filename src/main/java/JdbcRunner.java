5923ae2f6fd9314b6314c44d574f085c4af3926b
Skillnez <skillnezwork@gmail.com>
2025-03-17 19:22:05 +0300

    feat: implement CORS filter support

--- src/main/java/JdbcRunner.java
+++
@@ -1,6 +0,0 @@
-import java.sql.SQLException;
-
-public class JdbcRunner {
-    public static void main(String[] args) throws SQLException {
-    }
-}
---
+++ src/main/java/com/skillnez/filter/CorsFilter.java
@@ -0,0 +1,24 @@
+package com.skillnez.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CorsFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+}
