b6ac3e286cdaf22c4e4eb90b0151b6c2f217e92e
Skillnez <skillnezwork@gmail.com>
2025-03-29 20:58:09 +0300

    feat: implement Lombok

--- src/main/java/com/skillnez/model/entity/ExchangeRate.java
+++ src/main/java/com/skillnez/model/entity/ExchangeRate.java
@@ -1,82 +1,20 @@
 package com.skillnez.model.entity;
 
-import java.math.BigDecimal;
-import java.util.Objects;
+import lombok.*;
 
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
 public class ExchangeRate {
     private int id;
     private int baseCurrencyId;
     private int targetCurrencyId;
     private BigDecimal rate;
 
-    public ExchangeRate() {
-    }
-
-    public ExchangeRate(int id, int baseCurrencyId, int targetCurrencyId, BigDecimal rate) {
-        this.id = id;
-        this.baseCurrencyId = baseCurrencyId;
-        this.targetCurrencyId = targetCurrencyId;
-        this.rate = rate;
-    }
-
     public ExchangeRate(int baseCurrencyId, int targetCurrencyId, BigDecimal rate) {
         this.baseCurrencyId = baseCurrencyId;
         this.targetCurrencyId = targetCurrencyId;
         this.rate = rate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getBaseCurrencyId() {
-        return baseCurrencyId;
-    }
-
-    public void setBaseCurrencyId(int baseCurrencyId) {
-        this.baseCurrencyId = baseCurrencyId;
-    }
-
-    public int getTargetCurrencyId() {
-        return targetCurrencyId;
-    }
-
-    public void setTargetCurrencyId(int targetCurrencyId) {
-        this.targetCurrencyId = targetCurrencyId;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ExchangeRate that = (ExchangeRate) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "ExchangeRate{" +
-               "id=" + id +
-               ", baseCurrencyId=" + baseCurrencyId +
-               ", targetCurrencyId=" + targetCurrencyId +
-               ", rate=" + rate +
-               '}';
     }
 }
